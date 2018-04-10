b3e2d6d854be70a29e6eabac928e4cd4f286fbc3
Sean Furze <seanfjr02@gmail.com>
2018-04-10 09:13:49 -0400

    Removed Beta branch because it was not used often and hosted just a beta DLL

--- Update Mods.cmd
+++ Update Mods.cmd
@@ -7,17 +7,13 @@ C:
 cls
 echo 1. Update mods from outside of the Payday 2 Dir.
 echo 2. Update mods from outside of the Payday 2 Dir (D: ONLY! WORKING ON OTHER DRIVES SOON!).
-echo 3. Update mods to BETA BRANCH (C: drive only)
-echo 4. Update this script
-echo 5. Update this script (Beta version)
+echo 3. Update this script
 echo 99. Discord
 
 set /p a=
 IF %a%==1 goto 1Update
 IF %a%==2 goto 2start
-IF %a%==3 goto 3BETA
-IF %a%==4 goto 4UPDATE
-IF %a%==5 goto 5UPDATE
+IF %a%==3 goto 4UPDATE
 IF %a%==99 goto 99Discord
 
 :1Update
@@ -60,34 +56,11 @@ rmdir /s /q Payday-Modpack .git
 del "Update Mods.cmd" README.md
 exit
 
-:3BETA
-cls
-cd "C:\Program Files (x86)\Steam\steamapps\common\PAYDAY 2"
-rmdir /s /q mods assets\mod_overrides Maps
-del IPHLPAPI.dll
-git clone --branch beta https://github.com/46620/Payday-Modpack
-cd Payday-Modpack
-robocopy . .. /e >nul
-cd ..
-rmdir /s /q Payday-Modpack .git
-del "Update Mods.cmd" README.md
-exit
-
 :4UPDATE
 cls
 cd C:\Users\%USERNAME%\Desktop
 rmdir /s /q Payday-Modpack
 git clone --branch master https://github.com/46620/Payday-Modpack
-cd Payday-Modpack
-move "Update Mods.cmd" ..
-cd ..
-exit
-
-:5UPDATE
-cls
-cd C:\Users\%USERNAME%\Desktop
-rmdir /s /q Payday-Modpack
-git clone --branch beta https://github.com/46620/Payday-Modpack
 cd Payday-Modpack
 move "Update Mods.cmd" ..
 cd ..
